6838474ef9c8efa6ab3249d6cd8f60fb1c371e53
calebcheptumo <ckibusia@gmail.com>
2023-11-25 21:07:28 +0300

    configure URL routing

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.urls import path , include
+from rest_framework.routers import DefaultRouter
+from tasks.views import TaskViewSet
 
 # Create your views here.
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
